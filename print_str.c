45b82ae5a8ea7c57733f116334ba9b7e4b7f8008
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-07-12 15:21:38 +0100

    Emmanuel 5th Commit

--- print_str.c
+++ print_str.c
@@ -1,35 +1,22 @@
 #include "main.h"
 
 /**
- * print_str - prints a string
- * @str: string to be printed
- * Return: void
- */
-int print_substr(char *str)
-{
-	int i = 0;
-
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-		i++;
-	}
-	return (i);
-}
-
-/**
  * print_str - prints the string on screen
  * @ap: list to be evaluated
  * Return: void
  */
 int print_str(va_list ap)
 {
+	int count = 0;
 	char *str = va_arg(ap, char *);
-	
+
 	if (str == NULL)
 		str = "(nil)";
-	else if (*str == '\0')
-		return (-1);
-	return (print_substr(str));
+
+	while (*str != '\0')
+	{
+		count = count + _putchar(*str);
+		str++;
+	}
+	return (count);
 }
